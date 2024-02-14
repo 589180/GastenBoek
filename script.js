7bf65076f3475dc862c979000d9d9975c6e0c375
589180ROC <589180@edu.rocmn.nl>
2024-02-14 13:00:50 +0100

    Fixed Gast pagina

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-
 function openOverlay() {
-    document.getElementById("overlay").style.display = "flex";
+    document.querySelector(".overlay").style.display = "flex";
 }
 
 function closeOverlay() {
-    document.getElementById("overlay").style.display = "none";
+    document.querySelector(".overlay").style.display = "none";
 }
 
 document.getElementById("plusButton").addEventListener("click", openOverlay);
-
-const imageUpload = document.getElementById("image");
-const imageUploadArea = document.querySelector(".image-upload");
-
